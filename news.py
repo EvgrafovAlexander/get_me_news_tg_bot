19a3c3f62a15ce589ed57c43a168f0740278ebf5
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2025-11-18 17:29:49 +0500

    add volume

--- news.py
+++ news.py
@@ -1,9 +1,13 @@
 import feedparser
 import requests
 
+import os
 from io import BytesIO
 
 from db import is_article_new, mark_article_as_checked
+
+
+LAST_DAYS_ARTICLES = os.getenv("LAST_DAYS_ARTICLES")
 
 
 def get_news_from_source(url: str) -> list:
@@ -14,7 +18,7 @@ def get_news_from_source(url: str) -> list:
     for entry in feed.entries:
         title, link = entry.title, entry.link
         published_date, parsed_published_date = entry.published, entry.published_parsed
-        if is_article_new(link, parsed_published_date):
+        if is_article_new(link, parsed_published_date, LAST_DAYS_ARTICLES):
             news.append(
                 {
                     'title': title,
