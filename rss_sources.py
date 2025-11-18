dd7dfd4252504f351504d36bd0c9bf693dd25274
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2025-11-18 16:07:15 +0500

    add rss sources

--- rss_sources.py
+++ rss_sources.py
@@ -6,5 +6,61 @@ RSS_SOURCES = [
     {
         "name": "vc.ru",
         "url": "https://vc.ru/rss/tag/retail"
-    }
+    },
+    {
+        "name": "zhiza.evotor.ru",
+        "url": "https://zhiza.evotor.ru/rss/"
+    },
+    {
+        "name": "retailer.ru",
+        "url": "https://retailer.ru/news/rss/"
+    },
+    {
+        "name": "rbc.ru",
+        "url": "https://rssexport.rbc.ru/rbcnews/news/30/full.rss"
+    },
+    {
+        "name": "kommersant.ru",
+        "url": "https://www.kommersant.ru/rss/daily.xml"
+    },
+    {
+        "name": "vedomosti.ru/business/retail",
+        "url": "https://www.vedomosti.ru/rss/rubric/business/retail.xml"
+    },
+    {
+        "name": "publication.pravo.gov.ru/documents/daily",
+        "url": "http://publication.pravo.gov.ru/api/rss?pageSize=30"
+    },
+    {
+        "name": "cnews.ru",
+        "url": "https://www.cnews.ru/inc/rss/news.xml"
+    },
+    {
+        "name": "klerk.ru",
+        "url": "https://www.klerk.ru/export/news.rss"
+    },
+    {
+        "name": "t.me/crptbreaking",
+        "url": "https://rsshub.rssforever.com/telegram/channel/crptbreaking"
+    },
+    {
+        "name": "t.me/ofd24",
+        "url": "https://rsshub.rssforever.com/telegram/channel/ofd24"
+    },
+    {
+        "name": "t.me/markirovka_rf",
+        "url": "https://rsshub.rssforever.com/telegram/channel/markirovka_rf"
+    },
+    {
+        "name": "t.me/nalog_gov_ru",
+        "url": "https://rsshub.rssforever.com/telegram/channel/nalog_gov_ru"
+    },
+    {
+        "name": "t.me/itnewsru",
+        "url": "https://rsshub.rssforever.com/telegram/channel/itnewsru"
+    },
+    {
+        "name": "t.me/digitalretail",
+        "url": "https://rsshub.rssforever.com/telegram/channel/digitalretail"
+    },
 ]
